da214f485155eac290b73e44df933fad693b664f
SalamanderCtesiphon <swbrookshire@gmail.com>
2023-01-13 14:27:57 -0600

    completed the front-end part of the tutorial

--- react-crash-todo/src/App.js
+++ react-crash-todo/src/App.js
@@ -1,31 +1,62 @@
 import React, {Component} from "react";
 import './App.css';
 import Todos from './components/Todos';
+import Header from "./components/layout/Header";
+import AddTodo from "./components/AddTodo";
+import uniqid from "uniqid";
 
 class App extends Component {
   state = {
     todos: [
         {
-          id: 1,
+          id: uniqid(),
           title: 'Take out the trash',
           completed: false
         },
         {
-          id: 2,
+          id: uniqid(),
           title: 'Dinner with wife',
-          completed: true
+          completed: false
         },
         {
-          id: 3,
+          id: uniqid(),
           title: 'Meeting with boss',
           completed: false
         }
     ]
   }
+
+  markComplete = (id) => {
+    this.setState({ todos: this.state.todos.map(todo => {
+      if (todo.id === id) {
+        todo.completed = !todo.completed
+      }
+      return todo;
+    })})
+  }
+
+  delTodo = (id) => {
+    this.setState({ todos: [...this.state.todos.filter(todo => todo.id !==id)]})
+  }
+
+  addTodo = (title) => {
+    const newTodo = {
+      id: uniqid(),
+      title,
+      completed: false
+    }
+    this.setState({ todos: [...this.state.todos, newTodo] })
+  }
+
   render() {
     return (
       <div className="App">
-        <Todos todos={this.state.todos} />
+        <div className="container">
+          <Header />
+          <AddTodo addTodo={this.addTodo}/>
+          <Todos todos={this.state.todos} markComplete={this.markComplete} 
+          delTodo={this.delTodo} />
+        </div>
       </div>
     );
   }
