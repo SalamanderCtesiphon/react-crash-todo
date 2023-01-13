215c249fba89684f13529001850d9437d68984c6
SalamanderCtesiphon <swbrookshire@gmail.com>
2023-01-13 11:22:30 -0600

    update

---
+++ react-crash-todo/src/components/Todos.js
@@ -0,0 +1,33 @@
+import React, {Component} from "react";
+
+class Todos extends Component {
+    state = {
+        todos: [
+            {
+                id: 1,
+                title: 'Take out the trash',
+                completed: false
+            },
+            {
+                id: 2,
+                title: 'Dinner with wife',
+                completed: false
+            },
+            {
+                id: 3,
+                title: 'Meeting with boss',
+                completed: false
+            }
+        ]
+    }
+    render() {
+        console.log(this.state.todos)
+        return (
+            <div>
+                <h1>Todos</h1>
+            </div>
+        );
+    }
+}
+
+export default Todos;
