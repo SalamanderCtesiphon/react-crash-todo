a6d892e49159411eb4db0ca7a20fc2b38daea32f
SalamanderCtesiphon <swbrookshire@gmail.com>
2023-01-27 10:17:58 -0600

    continuation

---
+++ src/components/Tasks.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+const Tasks = ({ tasks }) => {
+   
+  return (
+    <div>
+        {tasks.map((task) => (
+            <h3 key={task.id}>{task.text}</h3>
+        ))}
+    </div>
+  )
+}
+
+export default Tasks
