c2ee47cc618e4ace9fa2b2168e2c5ca337e4fb2c
SalamanderCtesiphon <swbrookshire@gmail.com>
2023-01-27 14:04:38 -0600

    completed the tutorial

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import Header from './components/Header';
 import './index.css';
 import Tasks from './components/Tasks';
 import { useState } from 'react'
+import AddTask from './components/AddTask';
 
 const App = () =>  {
+  const [showAddTask, setShowAddTask] = useState(false)
   const [tasks, setTasks] = useState([
     {
         id: 1,
@@ -25,10 +27,30 @@ const App = () =>  {
     }
 ])
 
+const addTask = (task) => {
+  const id = Math.floor(Math.random() * 10000) + 1
+  const newTask = { id, ...task }
+  setTasks([...tasks, newTask])
+}
+
+const deleteTask = (id) => {
+  setTasks(tasks.filter((task) => task.id !== id ))
+}
+
+const toggleReminder = (id) => {
+  setTasks(tasks.map((task) => task.id === id ? {...task, reminder:
+  !task.reminder} : task
+  )
+  )
+}
+
   return (
     <div className="App container">
-      <Header />
-      <Tasks tasks={tasks} />
+      <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask}/>
+      {showAddTask && <AddTask onAdd={addTask}/>}
+      {tasks.length > 0 ? (<Tasks tasks={tasks} onDelete={deleteTask}
+      onToggle={toggleReminder} />)
+      : ( 'No Tasks To Show')}
     </div>
   )
 }
